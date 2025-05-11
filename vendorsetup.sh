b184e1482d201c2f48cd63b3141d3990233739ca
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-05-11 21:46:15 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_a12-user
-add_lunch_combo omni_a12-userdebug
-add_lunch_combo omni_a12-eng
+chmod a+x device/samsung/a12/mkbootimg
